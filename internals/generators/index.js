f1d96feadc15b92ea870c7774a88ba74743dff26
avetik <avetik.harutyunyan@logicify.com>
2018-01-05 11:18:31 +0200

    Create generator for redux reducers

--- internals/generators/index.js
+++ internals/generators/index.js
@@ -1,5 +1,7 @@
 const componentGenerator = require('./component');
+const reducerGenerator = require('./reducer');
 
 module.exports = function(plop) {
   plop.setGenerator('component', componentGenerator);
+  plop.setGenerator('reducer', reducerGenerator);
 };
---
+++ internals/generators/reducer/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  description: 'Redux reducer',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Reducer name',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../../src/reducers/{{ camelCase name }}Reducer.js',
+        templateFile: './reducer/templates/reducer.hbs',
+      }
+    ]
+};
