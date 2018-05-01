14fa27848f157e783cdb492dc197deb79d975543
VamOSGS <gegham.samvelyan@hotmail.com>
2018-05-01 22:06:32 +0500

    File-loader replaced with copy-plugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
-import './assets';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<Root />, document.getElementById('root'));
